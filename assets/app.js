2e2603990906811cb860d6e3a3e4d0f136c28d6c
Guillaume HESS <guillaume.hess@ccicampus.fr>
2026-03-25 14:22:07 +0100

    Premier test bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 import './stimulus_bootstrap.js';
 
 //IMPORT BOOTSTRAP
-import './bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@popperjs/core';
 
 //IMPORT CSS dans l'application
