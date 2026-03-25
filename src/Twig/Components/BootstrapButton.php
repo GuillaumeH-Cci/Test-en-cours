87f9e5b932b648e622e7d515dcd502bbc4a36120
Guillaume HESS <guillaume.hess@ccicampus.fr>
2026-03-25 14:59:14 +0100

    Test des boutons

--- src/Twig/Components/BootstrapButton.php
+++ src/Twig/Components/BootstrapButton.php
@@ -7,4 +7,7 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class BootstrapButton
 {
+    public string $text;
+    public string $type;
+    public string $link;
 }
