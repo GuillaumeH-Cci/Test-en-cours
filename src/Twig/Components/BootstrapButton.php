03c599be15944312aec24df885109442d3a227a1
Guillaume HESS <guillaume.hess@ccicampus.fr>
2026-03-26 09:29:08 +0100

    Création de l'encapsulation des boutons bootstrap

--- src/Twig/Components/BootstrapButton.php
+++ src/Twig/Components/BootstrapButton.php
@@ -7,7 +7,41 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class BootstrapButton
 {
-    public string $text;
-    public string $type;
-    public string $link;
+    private string $_strText;
+    private string $_strType;
+    private string $_strLink;
+
+    public function mount(string $text, string $type, string $link): void
+    {
+        $this->_strText = $text;
+        $this->_strType = $type;
+        $this->_strLink = $link;
+    }
+
+    /**
+     * Retourne le texte du bouton Bootstrap
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->_strText;
+    }
+
+    /**
+     * Retourne le type du bouton Bootstrap
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->_strType;
+    }
+
+    /**
+     * Retourne le lien du bouton Bootstrap
+     * @return string
+     */
+    public function getLink(): string
+    {
+        return $this->_strLink;
+    }
 }
