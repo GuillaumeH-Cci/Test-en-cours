9c3584025e5677b1e0f6d0c8c57fd473e9600973
Guillaume HESS <guillaume.hess@ccicampus.fr>
2026-03-26 13:46:09 +0100

    Integration of different customCapitalize

--- src/Twig/Runtime/AppExtensionRuntime.php
+++ src/Twig/Runtime/AppExtensionRuntime.php
@@ -15,13 +15,23 @@ class AppExtensionRuntime implements RuntimeExtensionInterface
      * On parcours tout le tableau et on check pour que chaque index soit par ou impair
      * @param $value - La chaine de caractère que l'on souhaite modifier
      */
-    public function customCapitalize($value)
+    public function customCapitalize($value, $bool = true)
     {
-        for ($i = 0; $i < strlen($value); $i++){
-            if ($i % 2) {
-                $value[$i] = strtolower($value[$i]);
-            } else {
-                $value[$i] = strtoupper($value[$i]);
+        if ($bool){ 
+            for ($i = 0; $i < strlen($value); $i++){
+                if ($i % 2) {
+                    $value[$i] = strtolower($value[$i]);
+                } else {
+                    $value[$i] = strtoupper($value[$i]);
+                }
+            }
+        } else {
+            for ($i = 0; $i < strlen($value); $i++){
+                if ($i % 2) {
+                    $value[$i] = strtoupper($value[$i]);
+                } else {
+                    $value[$i] = strtolower($value[$i]);
+                }
             }
         }
 
