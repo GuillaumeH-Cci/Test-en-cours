b0e909969d87989da74f9eb23ecd0f94415ca83c
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-03-27 15:44:51 +0100

    Modif prof pt3

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Pokemon;
+use App\Repository\PokemonRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,10 +13,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class PokemonController extends AbstractController
 {
     #[Route('/pokemon', name: 'app_pokemon')]
-    public function index(): Response
+    public function index(PokemonRepository $pokemonRepository): Response
     {
+        $arrPokemon = $pokemonRepository->findAll();
+
         return $this->render('pokemon/index.html.twig', [
-            'controller_name' => 'PokemonController',
+            'pokemonList' => $arrPokemon,
         ]);
     }
 
@@ -23,36 +26,64 @@ final class PokemonController extends AbstractController
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
         if($request->isMethod('POST')) {
-            $onError = false;
+
+            $onError = false; //< Flag qui vérifie si tout est OK ou pas
 
             $strPokemonName     = $request->request->get('name');
             $intPokemonNumber   = $request->request->get('number');
 
-            if ($intPokemonNumber <= 0) {
+            if($intPokemonNumber <= 0) {
 
                 $onError = true;
 
-                // Stockage du message pour l'erreur
-                $this->addFlash('danger', 'Le numéro du pokémon doit être supérieur à 0.');
+                // Je stocke un message "flash" dans la session
+                // Attention, flash nécessite une redirection propre '$this->redirectTo...'
+                // $this->addFlash('danger', "Le numéro dans le Pokédex doit être supérieur à zéro");
             }
 
-            if (!$onError){
+            if(!$onError) {
+
                 $objPokemon = new Pokemon();
 
-                $objPokemon ->setName($strPokemonName)
-                            ->setNumber($intPokemonNumber);
+                $objPokemon->setName($strPokemonName)
+                        ->setNumber($intPokemonNumber);
 
                 $entityManager->persist($objPokemon);
 
                 $entityManager->flush();
 
-                dd($objPokemon);
+                $this->addFlash('success', "Le Pokémon a été créé avec succès !");
+
+                // On redirige vers la page de détails du pokémon qui vient d'être enregistré en base
+                return $this->redirectToRoute('app_pokemon_show', [
+                    'id' => $objPokemon->getId()
+                ]);
             }
-            
         }
 
         return $this->render('pokemon/create.html.twig', [
             
+        ]);
+    }
+
+    #[Route('/pokemon/{id<\d+>}', name: 'app_pokemon_show')]
+    public function show(Pokemon $pokemon): Response
+    {
+        /*
+        $objPokemon = $pokemonRepository->find($id);
+
+        if(!$objPokemon) {
+            throw $this->createNotFoundException(
+                "Le Pokémon n'existe pas"
+            );
+        }
+        */
+
+        return $this->render('pokemon/show.html.twig', [
+            /*'pokemon_id'    => $id,
+            'pokemon'       => $objPokemon
+            */
+            'pokemon'       => $pokemon
         ]);
     }
 }
