bfea34aa95e4e17cc072731f5bdba8e21aa17f98
Guillaume HESS <guillaume.hess@ccicampus.fr>
2026-04-07 11:58:05 +0200

    Formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,32 +21,47 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('lastname')
-            ->add('firstname')
-            ->add('birthdate')
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom de Famille'
+            ])
+
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+
+            ->add('birthdate', DateType::class, [
+                'label' => 'Date de naissance'
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail'
+            ])
 
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Aggréer au termes d\'utilisation',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue(
-                        message: 'You should agree to our terms.',
+                        message: 'Vous devez aggréer au termes d\'utilisation du site.',
                     ),
                 ],
             ])
             
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Ce champs doit obligatoirement être remplis.',
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le Mot de passe'],
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank(
-                        message: 'Please enter a password',
+                        message: 'Entrer votre nouveau mot de passe',
                     ),
                     new Length(
                         min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
+                        minMessage: 'Votre mot de passe doit faire au moins {{ limit }} charactères',
                         // max length allowed by Symfony for security reasons
                         max: 4096,
                     ),
