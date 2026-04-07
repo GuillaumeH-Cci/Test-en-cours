605158c91ea1a188eb707efc292313eddeab24cc
Guillaume HESS <guillaume.hess@ccicampus.fr>
2026-04-07 09:54:16 +0200

    Modif Prof pt1.

--- src/Entity/Pokemon.php
+++ src/Entity/Pokemon.php
@@ -6,7 +6,7 @@ use App\Repository\PokemonRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Contraints as Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PokemonRepository::class)]
 #[ORM\Table(name: 'pokemons')]
@@ -20,6 +20,7 @@ class Pokemon
     #[ORM\Column(name: 'pkm_name', length: 160)]
     private ?string $name = null;
 
+    #[Assert\Positive]
     #[ORM\Column(name: 'pkm_number', nullable: true, unique: true)]
     private ?int $number = null;
 
@@ -27,10 +28,10 @@ class Pokemon
      * @var Collection<int, PokemonType>
      */
     #[Assert\Count(
-            min: 1,
-            max: 2,
-            minMessage: 'Un Pokémon doit avoir au moins {{ limit }} type',
-            maxMessage: 'Un Pokémon ne peut pas voir plus de {{ limit }} types',
+        min: 1,
+        max: 2,
+        minMessage: 'Un Pokémon doit avoir au moin {{ limit }} type',
+        maxMessage: 'Un Pokémon ne peut avoir plus de {{ limit }} types'
     )]
     #[ORM\JoinTable('pokemon_pokemon_types')]
     #[ORM\JoinColumn(name: 'ppt_pkm_id', referencedColumnName: 'pkm_id')]
