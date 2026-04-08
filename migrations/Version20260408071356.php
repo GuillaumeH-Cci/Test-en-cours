c5bd900eb8a3d0091284395a6f9a35fcbb32b0b7
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-08 09:14:21 +0200

    Merging - Je commence a comprendre

--- migrations/Version20260408071356.php
+++ migrations/Version20260408071356.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260408065857 extends AbstractMigration
+final class Version20260408071356 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerFJy52T1\App_KernelDevDebugContainer([
     'container.build_hash' => 'FJy52T1',
-    'container.build_id' => '42a46fad',
-    'container.build_time' => 1775631532,
+    'container.build_id' => '19f746d6',
+    'container.build_time' => 1775632434,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerFJy52T1');
