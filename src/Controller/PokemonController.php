d7b2294bbee3b8c46bb3c6eeb11d95a31e6fc637
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-08 08:40:50 +0200

    Pull

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Pokemon;
+use App\Form\PokemonCreateFormType;
 use App\Repository\PokemonRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,9 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/pokemon', name: 'app_pokemon_')]
 final class PokemonController extends AbstractController
 {
-    #[Route('/pokemon', name: 'app_pokemon')]
+    #[Route('/', name: 'index')]
     public function index(PokemonRepository $pokemonRepository): Response
     {
         $arrPokemon = $pokemonRepository->findAll();
@@ -22,68 +24,73 @@ final class PokemonController extends AbstractController
         ]);
     }
 
-    #[Route('/pokemon/create', name: 'app_pokemon_create')]
+    #[Route('/create', name: 'create')]
     public function create(Request $request, EntityManagerInterface $entityManager): Response
     {
-        if($request->isMethod('POST')) {
+        $objNewPokemon = new Pokemon();
 
-            $onError = false; //< Flag qui vérifie si tout est OK ou pas
+        $createForm = $this->createForm(PokemonCreateFormType::class, $objNewPokemon);
 
-            $strPokemonName     = $request->request->get('name');
-            $intPokemonNumber   = $request->request->get('number');
+        // J'envoi les données de la requête au formulaire
+        $createForm->handleRequest($request);
 
-            if($intPokemonNumber <= 0) {
+        // Vérifie si le formulaire est soumiii et que les données sont valides
+        if($createForm->isSubmitted() && $createForm->isValid()) {
 
-                $onError = true;
+            $entityManager->persist($objNewPokemon);
+            $entityManager->flush();
 
-                // Je stocke un message "flash" dans la session
-                // Attention, flash nécessite une redirection propre '$this->redirectTo...'
-                // $this->addFlash('danger', "Le numéro dans le Pokédex doit être supérieur à zéro");
-            }
+            // Affiche un message de succès
+            $this->addFlash('success', "Le pokémon a bien été créé en base");
 
-            if(!$onError) {
-
-                $objPokemon = new Pokemon();
-
-                $objPokemon->setName($strPokemonName)
-                        ->setNumber($intPokemonNumber);
-
-                $entityManager->persist($objPokemon);
-
-                $entityManager->flush();
-
-                $this->addFlash('success', "Le Pokémon a été créé avec succès !");
-
-                // On redirige vers la page de détails du pokémon qui vient d'être enregistré en base
-                return $this->redirectToRoute('app_pokemon_show', [
-                    'id' => $objPokemon->getId()
-                ]);
-            }
+            // Redirige vers la page de détails du pokémon créé
+            return $this->redirectToRoute('app_pokemon_show', [
+                'id' => $objNewPokemon->getId()
+            ]);
         }
-
-        return $this->render('pokemon/create.html.twig', [
-            
+        
+        return $this->render('pokemon/form.html.twig', [
+            'createForm'    => $createForm
         ]);
     }
 
-    #[Route('/pokemon/{id<\d+>}', name: 'app_pokemon_show')]
+    #[Route('/{id<\d+>}', name: 'show')]
     public function show(Pokemon $pokemon): Response
     {
-        /*
-        $objPokemon = $pokemonRepository->find($id);
-
-        if(!$objPokemon) {
-            throw $this->createNotFoundException(
-                "Le Pokémon n'existe pas"
-            );
-        }
-        */
-
         return $this->render('pokemon/show.html.twig', [
             /*'pokemon_id'    => $id,
             'pokemon'       => $objPokemon
             */
             'pokemon'       => $pokemon
+        ]);
+    }
+
+    #[Route('/{id<\d+>}/update', name: 'update')] //< URL : /pokemon/1/update
+    public function update(Pokemon $pokemon, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // On construit le formulaire à partir des données de l'entité récupérée
+        // depuis l'ID présent dans l'URL
+        $updateForm = $this->createForm(PokemonCreateFormType::class, $pokemon);
+
+        $updateForm->handleRequest($request);
+
+        if($updateForm->isSubmitted() && $updateForm->isValid()) {
+
+            // L'entité provenant déjà de la base, Doctrine la connait
+            // => pas besoin de persist
+
+            $entityManager->flush();
+
+            $this->addFlash('success', "Le pokémon a bien été modifié en base");
+
+            // Redirige vers la page de détails du pokémon modifié
+            return $this->redirectToRoute('app_pokemon_show', [
+                'id' => $pokemon->getId()
+            ]);
+        }
+
+        return $this->render('pokemon/form.html.twig', [
+            'createForm'    => $updateForm
         ]);
     }
 }
---
+++ src/Form/PokemonCreateFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pokemon;
+use App\Entity\PokemonType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PokemonCreateFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom du Pokémon'
+            ])
+
+            ->add('number', NumberType::class, [
+                'label' => 'Numéro du Pokédex nationnal'
+            ])
+
+            ->add('types', EntityType::class, [
+                'label'         => "Type(s)",
+                'class'         => PokemonType::class,  //< Classe utilisée pour les choix
+                'choice_label'  => 'name',              //< Attribut de l'objet utilisé pour le texte de l'option
+                'multiple'      => true,                //< Autorise la sélection multiple
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pokemon::class,
+        ]);
+    }
+}
