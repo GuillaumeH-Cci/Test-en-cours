3ce08c759a799857719c56b02539b44e2d5dc7c0
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-14 15:36:08 +0200

    ok

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerVxIMBxm\App_KernelDevDebugContainer([
     'container.build_hash' => 'VxIMBxm',
-    'container.build_id' => '8dd4cedf',
-    'container.build_time' => 1776170982,
+    'container.build_id' => '09216e49',
+    'container.build_time' => 1776172922,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerVxIMBxm');
